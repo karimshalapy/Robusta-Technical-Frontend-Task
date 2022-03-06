863fbaed58a08cfa2b555b858c3fc6f918b6afe1
Karim Shalapy <karimshalapy@gmail.com>
2022-03-06 19:58:56 +0200

    feat: use react-hook-form for validation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,30 +1,137 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import classes from './Form.module.css';
 import FormInput from './FormInput/FormInput';
 
+type Inputs = {
+  fullName: string,
+  email: string,
+  username: string,
+  password: string,
+  rePassword: string,
+};
+
 export const Form: FC = () => {
+  const methods = useForm<Inputs>({ mode: "onTouched" });
+  const { handleSubmit, watch } = methods
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data)
+  const password = useRef({})
+  password.current = watch("password", "")
+
+
   return (
-    <form className={classes.Form} aria-labelledby="signup">
-      <fieldset>
-        <legend id="signup">Sign-up</legend>
-        <FormInput type="text" name="fullName" id="full-name" required>
-          Full Name
-        </FormInput>
-        <FormInput type="email" name="email" id="email" required>
-          Email
-        </FormInput>
-        <FormInput type="text" name="username" id="username" required>
-          Username
-        </FormInput>
-        <FormInput type="password" name="password" id="password" required>
-          Password
-        </FormInput>
-        <FormInput type="password" name="rePassword" id="re-password" required>
-          Re-enter Password
-        </FormInput>
-      </fieldset>
-      <button id="submit" type="submit">Submit</button>
-    </form>
+    <FormProvider {...methods}>
+      <form className={classes.Form} aria-labelledby="signup" onSubmit={handleSubmit(onSubmit)}>
+        <fieldset>
+          <legend id="signup">Sign-up</legend>
+          <FormInput
+            type="text"
+            name="fullName"
+            id="full-name"
+            placeholder="Enter Your Full Name"
+            registerConfig={[
+              "fullName",
+              {
+                required: "This field is required",
+                pattern: {
+                  value: /^([^0-9]*)$/g,
+                  message: "Full name must not contain any numbers"
+                }
+              }
+            ]}
+            required
+          >
+            Full Name
+          </FormInput>
+          <FormInput
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter Your E-mail"
+            registerConfig={[
+              "email",
+              {
+                required: "This field is required",
+                pattern: {
+                  value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g,
+                  message: "Please, enter a valid email"
+                }
+              }
+            ]}
+            required
+          >
+            Email
+          </FormInput>
+          <FormInput
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter Your Username"
+            registerConfig={[
+              "username",
+              {
+                required: "This field is required",
+                pattern: {
+                  message: "Username must not contain special characters",
+                  value: /^[a-zA-Z0-9]*$/g
+                },
+                minLength: {
+                  value: 5,
+                  message: "Username must be at least 5 characters"
+                }
+              }
+            ]}
+            required
+          >
+            Username
+          </FormInput>
+          <FormInput
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter Your Password"
+            registerConfig={[
+              "password",
+              {
+                required: "This field is required",
+                pattern: {
+                  value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/g,
+                  message: "Password must contain at least one lowercase character, one uppercase character, and one number"
+                },
+                minLength: {
+                  value: 8,
+                  message: "Password must be at least 8 characters"
+                },
+                maxLength: {
+                  value: 32,
+                  message: "Password can't contain more than 32 characters"
+                }
+              }
+            ]}
+            required
+          >
+            Password
+          </FormInput>
+          <FormInput
+            type="password"
+            name="rePassword"
+            id="re-password"
+            placeholder="Re-enter Your Password"
+            registerConfig={[
+              "rePassword",
+              {
+                required: "This field is required",
+                validate: value => value === password.current || "The passwords don't match"
+              }
+            ]}
+            required
+          >
+            Re-enter Password
+          </FormInput>
+        </fieldset>
+        <button id="submit" type="submit">Submit</button>
+      </form>
+    </FormProvider>
   );
 }
 
--- src/components/Form/FormInput/FormInput.tsx
+++ src/components/Form/FormInput/FormInput.tsx
@@ -1,15 +1,24 @@
 import { FC, HTMLInputTypeAttribute, InputHTMLAttributes } from 'react'
+import { FieldValues, useFormContext, UseFormRegister } from 'react-hook-form'
 import classes from './FormInput.module.css'
 
 type Props = InputHTMLAttributes<HTMLInputElement> & {
     type: HTMLInputTypeAttribute
+    name: string
+    registerConfig: Parameters<UseFormRegister<FieldValues>>
 }
 
-export const FormInput: FC<Props> = ({ children, required, ...rest }) => {
+export const FormInput: FC<Props> = ({ children, registerConfig, ...props }) => {
+    const { register, formState: { errors } } = useFormContext()
+    const errorMessage = errors[props.name]?.message
+
     return (
         <label className={classes.FormInput}>
             {children}
-            <input {...rest} required={required} aria-required={required} />
+            <div className={classes.InputContainer}>
+                <input {...props} aria-required={props.required} aria-invalid={!!errorMessage} {...register.apply(this, registerConfig)} />
+                <span className={classes.Error} {...!!errorMessage ? { role: "alert" } : {}}>{errorMessage}</span>
+            </div>
         </label>
     )
 }
