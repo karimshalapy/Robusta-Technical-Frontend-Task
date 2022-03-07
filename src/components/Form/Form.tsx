d2c0a9e7eded32274d790556bff2820901de666b
Karim Shalapy <karimshalapy@gmail.com>
2022-03-07 07:49:37 +0200

    feat: apply basic styling to the form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -13,17 +13,20 @@ type Inputs = {
 
 export const Form: FC = () => {
   const methods = useForm<Inputs>({ mode: "onTouched" });
-  const { handleSubmit, watch } = methods
-  const onSubmit: SubmitHandler<Inputs> = data => console.log(data)
+  const { handleSubmit, watch, formState: { isValid } } = methods
   const password = useRef({})
   password.current = watch("password", "")
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data)
+
 
 
   return (
     <FormProvider {...methods}>
       <form className={classes.Form} aria-labelledby="signup" onSubmit={handleSubmit(onSubmit)}>
         <fieldset>
-          <legend id="signup">Sign-up</legend>
+          <legend id="signup">Create an Account</legend>
+          <p className={classes.Switch}>Already have an account? Log in <button>here</button></p>
+
           <FormInput
             type="text"
             name="fullName"
@@ -138,7 +141,7 @@ export const Form: FC = () => {
             Re-enter Password
           </FormInput>
         </fieldset>
-        <button id="submit" type="submit">Submit</button>
+        <button id="submit" type="submit" aria-disabled={!isValid} disabled={!isValid}>Submit</button>
       </form>
     </FormProvider>
   );
